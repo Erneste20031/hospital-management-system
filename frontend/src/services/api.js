650219c02fb37bb832f68c376f278d720a2dac02
Erneste20031 <ernestehagenimana04@gmail.com>
2026-06-03 10:31:33 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://hospital-backend-6ief.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
